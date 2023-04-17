91eb7073ae86b7cdbe07a9118713feb75c644d82
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-04-17 16:52:02 +0300

    Update.

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,43 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+           if(category.CategoryID !=0)
+            {
+                repo.Delete(category);
+            }
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
