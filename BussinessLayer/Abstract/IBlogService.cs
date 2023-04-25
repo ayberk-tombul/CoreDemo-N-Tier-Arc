3c24536fd78292cbc01d3f7551eb2e652cd365f3
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-04-25 19:00:33 +0300

    Update.

---
+++ BussinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogAdd(Category blog);
+        void BlogDelete(Category blog);
+        void BlogUpdate(Category blog);
+        List<Blog> GetList();
+        Blog GetById(int id);
+    }
+}
---
+++ BussinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void BlogAdd(Category blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogDelete(Category blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogUpdate(Category blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+    }
+}
